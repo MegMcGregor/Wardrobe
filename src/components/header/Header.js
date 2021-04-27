0979c79acf87d27436bbeaac1b40f4244917a127
MegMcGregor <megmcgregor1@gmail.com>
2021-04-27 15:59:36 -0500

    header JSX start

---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export const Header = () => {
+    return (
+        <>
+        <div className="header-container">
+
+            <div classNme="top-header">
+                <a href="/">Logout</a>
+                <img alt="user icon"></img>
+            </div>
+
+            <div className="bottom-header">
+                <div class="logo-container">
+                    <img alt="logo"></img>
+                </div>
+
+                
+            </div>
+
+        </div>
+        </>
+    )
+}
