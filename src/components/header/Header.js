c0e7aefd68bc9b99c355758c342f98766ffc90fd
MegMcGregor <megmcgregor1@gmail.com>
2021-04-27 21:01:58 -0500

    header JSX and CSS

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,35 @@
 import React from "react"
+import "./Header.css" 
+import img from "./Wardrobe.png"
 
 export const Header = () => {
     return (
         <>
         <div className="header-container">
 
-            <div classNme="top-header">
-                <a href="/">Logout</a>
-                <img alt="user icon"></img>
+            <div className="top-header">
+                    <a href="/">Logout</a>
+                    <img className="user-icon"></img>
             </div>
 
             <div className="bottom-header">
-                <div class="logo-container">
-                    <img alt="logo"></img>
+
+                <div className="logo-container">
+                    <img className="logo" alt="logo" src={img}></img>
                 </div>
 
-                
+                <div className="title-container">
+                    <h1>Wardrobe</h1>
+                </div> 
+
+                <div className="nav-container">
+                    <nav></nav>
+                </div>
+
             </div>
 
         </div>
+
         </>
     )
 }
