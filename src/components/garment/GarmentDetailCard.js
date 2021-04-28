53a41b3e9889b967e49f35f71060925534bf1edd
MegMcGregor <megmcgregor1@gmail.com>
2021-04-28 13:56:27 -0500

    detail card file setup

---
+++ src/components/garment/GarmentDetailCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./GarmentDetailCard.css"
+
+export const GarmentDetail = () => {
+return (
+    <>
+    <div className="detail-card-border">
+    </div>
+    </>
+)
+}
