9657ce42e84e3342ef6f9d3705550326b2601f84
MegMcGregor <megmcgregor1@gmail.com>
2021-05-06 11:19:31 -0500

    restored color functionality

--- src/components/garment/GarmentForm.js
+++ src/components/garment/GarmentForm.js
@@ -31,6 +31,7 @@ export const GarmentForm = () => {
 
     const [images, setImages] = useState([]);
     const [colors, setColors] = useState([]);
+    const [selectedColor, setSelectedColor] = useState([]);
     const [types, setTypes] = useState([]);
     const [occasions, setOccasions] = useState([]);
     const [purchaseDates, setPurchaseDates] = useState([])
@@ -54,180 +55,179 @@ export const GarmentForm = () => {
     const handleColorChange = (event) => {
         event.preventDefault()
         const selectedColor = event.target.id
-        console.log(selectedColor)
-    }
+        setSelectedColor(selectedColor)
 
-
-    const handleClickSaveGarment = (event) => {
-        event.preventDefault()
-
-        // const colorId = garment.colorId
-        const typeId = garment.typeId;
-        const conditionId = garment.conditionId
-        const occasionId = garment.occasionId
-        const purchaseDate = garment.purchaseDateId
-        const seasonId = garment.seasonId
-        const brand = garment.brand
-        const composition = garment.composition
-        const title = garment.title
-
-
-        if (typeId === 0 || conditionId === 0 || occasionId === 0) {
-            window.alert("Please complete the form before submitting");
-        } else {
-            addGarment(garment)
-                .then(() => history.push("/garments"));
-        }
     }
 
 
 
-    useEffect(() => {
-        getAllTypes()
-            .then(typesFromAPI => {
-                setTypes(typesFromAPI)
-            })
-    }, []);
+const handleClickSaveGarment = (event) => {
+    event.preventDefault()
 
-    useEffect(() => {
-        getAllColors()
-            .then(colorsFromAPI => {
-                setColors(colorsFromAPI)
-            })
-    }, []);
+    const colorId = garment.colorId
+    const typeId = garment.typeId;
+    const conditionId = garment.conditionId
+    const occasionId = garment.occasionId
+    const purchaseDate = garment.purchaseDateId
+    const seasonId = garment.seasonId
+    const brand = garment.brand
+    const composition = garment.composition
+    const title = garment.title
 
 
-    useEffect(() => {
-        getAllOccasions()
-            .then(occasionsFromAPI => {
-                setOccasions(occasionsFromAPI)
-            })
-    }, []);
-
-    useEffect(() => {
-        getAllConditions()
-            .then(conditionsFromAPI => {
-                setConditions(conditionsFromAPI)
-            })
-    }, []);
-
-    useEffect(() => {
-        getAllSeasons()
-            .then(seasonsFromAPI => {
-                setSeasons(seasonsFromAPI)
-            })
-    }, []);
-
-    useEffect(() => {
-        getAllPurchaseDates()
-            .then(datesFromAPI => {
-                setPurchaseDates(datesFromAPI)
-            })
-    }, []);
+    if (typeId === 0 || conditionId === 0 || occasionId === 0) {
+        window.alert("Please complete the form before submitting");
+    } else {
+        addGarment(garment)
+            .then(() => history.push("/garments"));
+    }
+}
 
 
 
+useEffect(() => {
+    getAllTypes()
+        .then(typesFromAPI => {
+            setTypes(typesFromAPI)
+        })
+}, []);
 
-    return (
-        <div className="main-container">
+useEffect(() => {
+    getAllColors()
+        .then(colorsFromAPI => {
+            setColors(colorsFromAPI)
+        })
+}, []);
 
-            <div className="page-title-container">
-                <h2>
-                    new item
+
+useEffect(() => {
+    getAllOccasions()
+        .then(occasionsFromAPI => {
+            setOccasions(occasionsFromAPI)
+        })
+}, []);
+
+useEffect(() => {
+    getAllConditions()
+        .then(conditionsFromAPI => {
+            setConditions(conditionsFromAPI)
+        })
+}, []);
+
+useEffect(() => {
+    getAllSeasons()
+        .then(seasonsFromAPI => {
+            setSeasons(seasonsFromAPI)
+        })
+}, []);
+
+useEffect(() => {
+    getAllPurchaseDates()
+        .then(datesFromAPI => {
+            setPurchaseDates(datesFromAPI)
+        })
+}, []);
+
+
+return (
+    <div className="main-container">
+
+        <div className="page-title-container">
+            <h2>
+                new item
                 </h2>
-            </div>
-            <form className="form-border">
-            {/* value={selectedColor} */}
-                <div className="form-header-border">
-                    <div className="form-top-left">
-                        <span className="header-dot" ></span>
-                        <div className="title-input">
-                            <input type="text" id="title" placeholder="title" value={garment.title} onChange={handleControlledInputChange} />
-                        </div>
-                    </div>
-                    <div className="form-top-right">
-                        <select className="image-menu">
-                            <option></option>
-                        </select>
-                    </div>
-                </div>
-
-                <div className="form-middle-border">
-                    <div className="color-container">
-                        <button id="red" className="dot" onClick={handleColorChange}></button>
-                        <button id="orange" className="dot"></button>
-                        <button id="yellow" className="dot"></button>
-                        <button id="green" className="dot"></button>
-                        <button id="blue" className="dot"></button>
-                        <button id="purple" className="dot"></button>
-                        <button id="black" className="dot"></button>
-                        <button id="white" className="dot"></button>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="brand-name-input">brand:</label>
-                        <input type="text" id="brand" placeholder="brand" value={garment.brand} onChange={handleControlledInputChange} />
-
-                        <label htmlFor="type-select">type:</label>
-                        <select value={garment.typeId} name="typeId" id="typeId" onChange={handleControlledInputChange} className="form-menu">
-                            <option value="0">select a type</option>
-                            {types.map(type => (
-                                <option key={type.id} value={type.id}>
-                                    {type.name}
-                                </option>
-                            ))}
-                        </select>
-
-                        <label htmlFor="season-select">season:</label>
-                        <select value={garment.seasonId} name="seasonId" id="seasonId" onChange={handleControlledInputChange} className="form-menu">
-                            <option value="0">select a type</option>
-                            {seasons.map(season => (
-                                <option key={season.id} value={season.id}>
-                                    {season.name}
-                                </option>
-                            ))}
-                        </select>
-
-                        <label htmlFor="occasion-select">occasion:</label>
-                        <select value={garment.occasionId} name="occasionId" id="occasionId" className="form-menu" onChange={handleControlledInputChange}>
-                            <option value="0">occasion</option>
-                            {occasions.map(occasion => (
-                                <option key={occasion.id} value={occasion.id}>
-                                    {occasion.name}
-                                </option>
-                            ))}
-                        </select>
-
-                        <label htmlFor="condition-select">condition:</label>
-                        <select value={garment.conditionId} name="conditionId" id="conditionId" className="form-menu" onChange={handleControlledInputChange}>
-                            <option value="0">condition</option>
-                            {conditions.map(condition => (
-                                <option key={condition.id} value={condition.id}>
-                                    {condition.name}
-                                </option>
-                            ))}
-                        </select>
-
-                        <label htmlFor="compsition-input">composition:</label>
-                        <input id="composition" type="text" placeholder="composition" value={garment.composition} onChange={handleControlledInputChange} />
-
-                        <label htmlFor="purchase-date-select">purchase date:</label>
-                        <select value={garment.purchaseDateId} id="purchaseDate" className="form-menu" onChange={handleControlledInputChange}>
-                            <option>date</option>
-                            {purchaseDates.map(purchaseDate => (
-                                <option key={purchaseDate.id} value={purchaseDate.id}>
-                                    {purchaseDate.purchaseDate}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
-
-                <div className="form-footer-border">
-                    <a className="form-back" href="/">back</a>
-                    <button className="form-save" onClick={handleClickSaveGarment}>save</button>
-                </div>
-
-            </form>
         </div>
-    )
+        <form className="form-border">
+            <div className="form-header-border">
+                <div className="form-top-left">
+                    <span className="header-dot" id={selectedColor}></span>
+                    <div className="title-input">
+                        <input type="text" id="title" placeholder="title" value={garment.title} onChange={handleControlledInputChange} />
+                    </div>
+                </div>
+                <div className="form-top-right">
+                    <select className="image-menu">
+                        <option></option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="form-middle-border">
+                <div className="color-container">
+                    <button value={garment.colorId} id="red" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="orange" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="yellow" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="green" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="blue" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="purple" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="black" className="dot" onClick={handleColorChange}></button>
+                    <button value={garment.colorId} id="white" className="dot" onClick={handleColorChange}></button>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="brand-name-input">brand:</label>
+                    <input type="text" id="brand" placeholder="brand" value={garment.brand} onChange={handleControlledInputChange} />
+
+                    <label htmlFor="type-select">type:</label>
+                    <select value={garment.typeId} name="typeId" id="typeId" onChange={handleControlledInputChange} className="form-menu">
+                        <option value="0">select a type</option>
+                        {types.map(type => (
+                            <option key={type.id} value={type.id}>
+                                {type.name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label htmlFor="season-select">season:</label>
+                    <select value={garment.seasonId} name="seasonId" id="seasonId" onChange={handleControlledInputChange} className="form-menu">
+                        <option value="0">select a type</option>
+                        {seasons.map(season => (
+                            <option key={season.id} value={season.id}>
+                                {season.name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label htmlFor="occasion-select">occasion:</label>
+                    <select value={garment.occasionId} name="occasionId" id="occasionId" className="form-menu" onChange={handleControlledInputChange}>
+                        <option value="0">occasion</option>
+                        {occasions.map(occasion => (
+                            <option key={occasion.id} value={occasion.id}>
+                                {occasion.name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label htmlFor="condition-select">condition:</label>
+                    <select value={garment.conditionId} name="conditionId" id="conditionId" className="form-menu" onChange={handleControlledInputChange}>
+                        <option value="0">condition</option>
+                        {conditions.map(condition => (
+                            <option key={condition.id} value={condition.id}>
+                                {condition.name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label htmlFor="compsition-input">composition:</label>
+                    <input id="composition" type="text" placeholder="composition" value={garment.composition} onChange={handleControlledInputChange} />
+
+                    <label htmlFor="purchase-date-select">purchase date:</label>
+                    <select value={garment.purchaseDateId} id="purchaseDateId" className="form-menu" onChange={handleControlledInputChange}>
+                        <option>date</option>
+                        {purchaseDates.map(purchaseDate => (
+                            <option key={purchaseDate.id} value={purchaseDate.id}>
+                                {purchaseDate.purchaseDate}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+
+            <div className="form-footer-border">
+                <a className="form-back" href="/">back</a>
+                <button className="form-save" onClick={handleClickSaveGarment}>save</button>
+            </div>
+
+        </form>
+    </div>
+)
 }
